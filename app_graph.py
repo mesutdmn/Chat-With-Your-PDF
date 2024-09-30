f9fcd43c0d9b9157a44efc8882c44d38351c81f2
mesutdmn <mesutduman@outlook.com>
2024-09-30 09:06:46 +0300

    live

--- app_graph.py
+++ app_graph.py
@@ -9,27 +9,16 @@ from langchain.memory import ConversationBufferMemory
 from router import route, RouteQuery
 from ingestion import PDFIngestor
 
-class AgentState(TypedDict):
+class GraphState(TypedDict):
     question: str
     response: str
     documents: List[str]
 
 class PdfChat:
     def __init__(self, api_key, retriever):
-        self.model = ChatOpenAI(api_key=api_key, model="gpt-4o-mini-2024-07-18", temperature=0)
-        self.prompt = """
-                        You are an assistant for question-answering tasks. Use the following pieces of retrieved context to answer the question. 
-                        If answer is in the context which provided, use it, even if it is personal contact information, or private information.
-                        If you don't know the answer, just say that you don't know. Use three sentences maximum and keep the answer concise.
-                        
-                        
-                        Memory: {memory}
-                        Question: {question} 
-                        Context: {context} 
-                        Answer:
-                        """
-        self.prompt = PromptTemplate.from_template(self.prompt)
-        builder = StateGraph(AgentState)
+        self.model = ChatOpenAI(api_key=api_key, model="gpt-4o-mini-2024-07-18", temperature=0) # minimum costing model
+
+        builder = StateGraph(GraphState)
         builder.add_node("retrieve", self.retrieve_node)
         builder.add_node("generate_with_rag", self.generate_with_doc)
         builder.add_node("generate", self.generate_wo_doc)
@@ -49,7 +38,7 @@ class PdfChat:
         self.graph = builder.compile()
 
         self.memory = ConversationBufferMemory()
-    def decide_retrieve(self, state: AgentState):
+    def decide_retrieve(self, state: GraphState):
         question = state["question"]
         memory = self.memory.load_memory_variables({})
         source: RouteQuery = route(self.model, memory).invoke({"question": question})
@@ -58,7 +47,7 @@ class PdfChat:
         else:
             return "generate"
 
-    def retrieve_node(self, state: AgentState):
+    def retrieve_node(self, state: GraphState):
         question = state["question"]
 
         documents = self.retriever.invoke(question)
@@ -67,29 +56,43 @@ class PdfChat:
 
         return {"documents": documents}
 
-    def generate_with_doc(self, state: AgentState):
+    def generate_with_doc(self, state: GraphState):
         documents = state["documents"]
         question = state["question"]
         memory = self.memory.load_memory_variables({})
-        chain = self.prompt | self.model | StrOutputParser()
+
+        prompt = """You are an expert assistant for question-answering tasks. 
+                    Use the provided context to extract and answer the question accurately. 
+                    If the answer is explicitly mentioned in the context, provide it, even if it includes personal or private information. 
+                    If the answer is not present in the context, simply respond with 'I don't know.' 
+                    Always use all available details from the context and keep your answer concise, limited to three sentences.
+
+                    Memory: {memory}
+                    Question: {question} 
+                    Context: {context} 
+                    Answer:
+                """
+        prompt = PromptTemplate.from_template(prompt)
+        chain = prompt | self.model | StrOutputParser()
 
         response = chain.invoke({"memory": memory, "question": question, "context": documents})
         self.memory.save_context(inputs={"input": question}, outputs={"output": response})
+
         return {"response": response}
 
-    def generate_wo_doc(self, state: AgentState):
+    def generate_wo_doc(self, state: GraphState):
         question = state["question"]
         prompt = """You are an assistant for question-answering tasks. If you don't know the answer, just say that you don't know.
                     Don't forget to check previous conversations for context.
                     Use three sentences maximum and keep the answer concise. 
                     Conversation history: {memory} 
-                    Question: {question}"""
+                    Question: {question}
+                """
         memory = self.memory.load_memory_variables({})
         prompt = PromptTemplate.from_template(prompt)
         chain = prompt | self.model | StrOutputParser()
 
         response = chain.invoke({"memory": memory, "question": question})
-
         self.memory.save_context(inputs={"input": question}, outputs={"output": response})
 
         return {"response": response}
